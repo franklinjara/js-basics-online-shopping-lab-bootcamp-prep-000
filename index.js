e2b43e85dbdd511428ace69c8b0e1a12510fd35d
Franklin <makyfj@hotmail.com>
2019-04-11 02:48:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,38 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price = {[item]: Math.ceil( Math.random() * 100)};
+ cart.push({[item] : price});
+ console.log(`${item} has been added to your car`);
+ return cart;
 }
 
 function viewCart() {
   // write your code here
+  if(cart.length === 0) {
+    return `Your shopping cart is empty.`;
+  }
+  
+  var itemsWithPrices = [];
+  
+  for (var i = 0; i < cart.length; i++) {
+    var keys = Object.keys(cart[i])[0];
+    itemsWithPrices.push(keys + "at $" + cart[i][keys]);
+    
+    var myString = "In your cart, you have";
+    
+    if(itemsWithPrices.length === 1) {
+      myString += itemsWithPrices + ".";
+    } else if(itemsWithPrices.length === 2) {
+      myString += itemsWithPrices[0] + ", and " + itemsWithPrices[1] + ".";
+    } else if(itemsWithPrices.length > 2){
+      var lastElement = itemsWithPrices.pop();
+      var otherItem = itemsWithPrices.join(", ");
+      myString += (`${otherItem} and ${lastElement}.`);
+    }
+    console.log(myString);
+  }
+    
 }
 
 function total() {
